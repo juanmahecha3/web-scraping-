5ed61c45a16e0b01533879e42d992dc4ecd7e243
juanmahecha3 <juanmahecha3@gmail.com>
2023-10-13 21:15:43 -0500

    cambios 2

--- practica.py
+++ practica.py
@@ -1,3 +1,7 @@
-# Importar BeutifulSoup
+# Importar BeutifulSoup y requests
 from bs4 import BeautifulSoup
 import requests
+
+import bs4 # solo para chequeo
+print(" Versión de BeatifulSoup: ", bs4._vesion_)
+print(" Versión de requests: ", requests._vesion_)
