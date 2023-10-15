f9e5d0ab594700acb00176851cd5638387a38e34
juanmahecha3 <juanmahecha3@gmail.com>
2023-10-15 18:08:14 -0500

    Cambios imagenes

--- imagenes.py
+++ imagenes.py
@@ -1,15 +1,25 @@
-# Importar BeutifulSoup y requests
 from bs4 import BeautifulSoup
 import requests
-# Empezamos en Scraping
-# 1 Obtener el HTML
 URL_BASE = 'https://scrapepark.org/courses/spanish/'
 pedido_obtenido = requests.get(URL_BASE)
 html_obtenido =  pedido_obtenido.text
-# 2. "Parsear" ese HTML
 soup1 = BeautifulSoup(html_obtenido,"html.parser")
-
 src_todos = soup1.find_all(src=True)
+#Etiquetas Imagenes
 for elemento in src_todos:
     if (elemento['src'].endswith(".png")) or (elemento['src'].endswith("jpg")):
         print(elemento)
+#Almacenar imagenes
+url_imagenes =[]
+for i, imagen in enumerate(src_todos):
+    if imagen['src'].endswith("png"):
+        print(imagen['src'])
+        r = requests.get(f"https://scrapepark.org/courses/spanish/{imagen['src']}")
+        with open(f'imagenes/imagen_{i}.png','wb') as f:
+            f.write(r.content)
+
+    if imagen['src'].endswith("jpg"):
+        print(imagen['src'])
+        r = requests.get(f"https://scrapepark.org/courses/spanish/{imagen['src']}")
+        with open(f'imagenes/imagen_{i}.jpg','wb') as f:
+            f.write(r.content)
