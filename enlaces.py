83b6f4183d065e261000eb4226650a76ea36f836
juanmahecha3 <juanmahecha3@gmail.com>
2023-10-15 20:18:10 -0500

    guardar enlaces

---
+++ enlaces.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+URL_BASE = 'https://scrapepark.org/courses/spanish/'
+pedido_obtenido = requests.get(URL_BASE)
+html_obtenido =  pedido_obtenido.text
+# 2. "Parsear" ese HTML
+soup = BeautifulSoup(html_obtenido,"html.parser")
+
+menu = soup.find(string=re.compile("MENÚ"))
+print(menu.parent.find_next_siblings())
